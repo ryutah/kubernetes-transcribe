559523f26df1234f0c06446bf927ac6574d45ddd
ryutah <ryuta0221@gmail.com>
2018-07-21 16:58:55 +0900

    Update: registry to include endpoint.Registry

--- pkg/registry/service/registry.go
+++ pkg/registry/service/registry.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/ryutah/kubernetes-transcribe/pkg/api"
 	"github.com/ryutah/kubernetes-transcribe/pkg/labels"
+	"github.com/ryutah/kubernetes-transcribe/pkg/registry/endpoint"
 	"github.com/ryutah/kubernetes-transcribe/pkg/watch"
 )
 
@@ -14,4 +15,8 @@ type Registry interface {
 	DeleteService(name string) error
 	UpdateService(svc *api.Service) error
 	WatchServices(labels, fields labels.Selector, resourceVersion uint64) (watch.Interface, error)
+
+	// TODO: endpoints and their implementation should be separated, setting endpoints should be
+	// supported via the API, and the endpoints-controller should use the API to update endpoints.
+	endpoint.Registry
 }
